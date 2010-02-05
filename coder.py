902c3787e23fcf7854d1152395364240ca028eb9
John <john@jjmurph.com>
2010-02-04 20:18:50 -0500

    added open/save dialog boxes added ability to close a tab

--- coder.py
+++ coder.py
@@ -23,7 +23,7 @@ class TextEditor(object):
         #gtk objects from glade
         self.window = builder.get_object("window")
         self.notebook = builder.get_object("notebook")
-        
+       
         scrolledwindow = builder.get_object("scrolledwindow")
         textview = builder.get_object("textview")
         #label = builder.get_object("label")
@@ -56,8 +56,34 @@ class TextEditor(object):
         self.new_tab()
     
     def on_menu_item_open_activate(self,widget,data=None):
-        filename = os.getcwd() + os.sep + 'testfile.txt'
-        self.load_file(filename)
+        file_chooser = gtk.FileChooserDialog(
+                        title = 'Open',
+                        action = gtk.FILE_CHOOSER_ACTION_OPEN,
+                        buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
+                                   gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+        response = file_chooser.run()
+        if response == gtk.RESPONSE_ACCEPT:
+            filename = file_chooser.get_filename()
+            self.load_file(filename)
+        file_chooser.destroy()
+
+    def on_menu_item_save_activate(self,widget,data=None):
+        tab = self.current_tab()
+        filename = tab.get_filename()
+        self.save_file(filename)
+
+    def on_menu_item_save_as_activate(self,widget,data=None):
+        file_chooser = gtk.FileChooserDialog(
+                        title = 'Save as',
+                        action = gtk.FILE_CHOOSER_ACTION_SAVE,
+                        buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
+                                   gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+        response = file_chooser.run()
+        if response == gtk.RESPONSE_ACCEPT:
+            filename = file_chooser.get_filename()
+            self.save_file(filename)
+        file_chooser.destroy()
+
         
     ###########################
 
@@ -98,6 +124,38 @@ class TextEditor(object):
             print("Loaded %s" % filename)
         except IOError as e:          
             print("Couldn't open file %s" % filename)
+
+    def save_file(self,filename):
+        if filename:
+            tab = self.current_tab()
+            textview = tab.get_textview()
+            textbuffer = textview.get_buffer()
+            start = textbuffer.get_start_iter()
+            end = textbuffer.get_end_iter()
+
+            # get the full text in the buffer
+            text = textbuffer.get_text(start,end)
+           
+            #TODO: check if file exists
+
+            try:
+                f = open(filename,'w')
+                f.write(text)
+                f.close()
+
+                # reset the modified flag
+                textbuffer.set_modified(False)
+
+                #update the filename and label of the tab
+                tab.set_filename(filename)
+
+                print("Saved %s" % filename)
+            except IOError as e:
+                print("Error saving file %s" % filename)
+
+    def close_tab(self):
+        self.notebook.remove_page(self.current_page())
+        self.tabs.remove(tab)
 
 
 class Tab(object):
