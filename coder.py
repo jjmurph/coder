67faab154562ce86638075fe623ea064ce47c1d8
John <john@jjmurph.com>
2010-02-04 20:18:17 -0500

    can now load files from the command line

--- coder.py
+++ coder.py
@@ -4,6 +4,102 @@ import sys
 import gtk
 import os
 
+
+class TextEditor(object):
+    '''
+    The main GUI of the application.
+    A window with a tabbed notebook interface
+    '''
+
+    def __init__(self,filenames=[]):
+        '''
+        Loads the Glade file and sets up the GUI.
+        Accepts an optional list of filenames to open
+        '''
+
+        builder = gtk.Builder()
+        builder.add_from_file("coder.glade")
+        
+        #gtk objects from glade
+        self.window = builder.get_object("window")
+        self.notebook = builder.get_object("notebook")
+        
+        scrolledwindow = builder.get_object("scrolledwindow")
+        textview = builder.get_object("textview")
+        #label = builder.get_object("label")
+        # this should be in the Glade file
+        label = gtk.Label("New Document")
+
+        #link the signal handlers
+        builder.connect_signals(self)
+       
+        self.tabs = [] #list of all active tabs
+        tab = Tab(self.notebook,scrolledwindow,textview,label)
+        self.tabs.append(tab)
+    
+        if filenames:
+            self.load_file(filenames[0])
+            filenames = filenames[1:]
+            # if there was more than 1 filename then
+            # create tabs for each one and load the files
+            for f in filenames:
+                self.new_tab()
+                self.load_file(f)
+
+        
+    ##### signal handlers #####
+
+    def on_window_destroy(self,widget,data=None):
+        gtk.main_quit()
+        
+    def on_menu_item_new_activate(self,widget,data=None):
+        self.new_tab()
+    
+    def on_menu_item_open_activate(self,widget,data=None):
+        filename = os.getcwd() + os.sep + 'testfile.txt'
+        self.load_file(filename)
+        
+    ###########################
+
+    def current_page(self):
+        return self.notebook.get_current_page()
+
+    def current_tab(self):
+        return self.tabs[self.current_page()]
+
+    def new_tab(self):
+        tab = Tab(self.notebook)
+        self.tabs.append(tab)
+        self.notebook.set_current_page(len(self.tabs)-1)
+    
+    def load_file(self,filename):
+        try:
+            f = open(filename,'r')
+            text = f.read()
+            f.close()
+            
+            tab = self.current_tab()
+            textview = tab.get_textview()
+            textbuffer = textview.get_buffer()
+            
+            #disable the text view while loading the buffer
+            textview.set_sensitive(False)
+            
+            #store the file's contents in the buffer
+            textbuffer.set_text(text)
+            textbuffer.set_modified(False)
+            
+            #turn the text view back on
+            textview.set_sensitive(True)
+          
+            #update the filename and label of the tab
+            tab.set_filename(filename)
+           
+            print("Loaded %s" % filename)
+        except IOError as e:          
+            print("Couldn't open file %s" % filename)
+
+
 class Tab(object):
     '''
     Manages the Scrolled Window, Text View, Text Buffer, Label, and File
@@ -58,87 +154,19 @@ class Tab(object):
         self.filename = filename
         self.label.set_text(os.path.basename(filename))
         self.notebook.set_tab_label(self.window,self.label)
- 
 
-class TextEditor(object):
-    def __init__(self):
-        builder = gtk.Builder()
-        builder.add_from_file("coder.glade")
-        
-        #gtk objects from glade
-        self.window = builder.get_object("window")
-        self.notebook = builder.get_object("notebook")
-        
-        scrolledwindow = builder.get_object("scrolledwindow")
-        textview = builder.get_object("textview")
-        #label = builder.get_object("label")
-        label = gtk.Label("New Document")
 
-        #link the signal handlers
-        builder.connect_signals(self)
-       
-        self.tabs = [] #list of all active tabs
-        tab = Tab(self.notebook,scrolledwindow,textview,label)
-        self.tabs.append(tab)
-        
-    ##### signal handlers #####
-
-    def on_window_destroy(self,widget,data=None):
-        gtk.main_quit()
-        
-    def on_menu_item_new_activate(self,widget,data=None):
-        self.new_tab()
-        self.notebook.set_current_page(len(self.tabs)-1)
-    
-    def on_menu_item_open_activate(self,widget,data=None):
-        filename = os.getcwd() + os.sep + 'testfile.txt'
-        self.load_file(filename)
-        
-    ###########################
-
-    def current_page(self):
-        return self.notebook.get_current_page()
-
-    def current_tab(self):
-        return self.tabs[self.current_page()]
-
-    def new_tab(self):
-        tab = Tab(self.notebook)
-        self.tabs.append(tab)
-    
-    def load_file(self,filename):
-        try:
-            f = open(filename,'r')
-            text = f.read()
-            f.close()
-            
-            tab = self.current_tab()
-            textview = tab.get_textview()
-            textbuffer = textview.get_buffer()
-            
-            #disable the text view while loading the buffer
-            textview.set_sensitive(False)
-            
-            #store the file's contents in the buffer
-            textbuffer.set_text(text)
-            textbuffer.set_modified(False)
-            
-            #turn the text view back on
-            textview.set_sensitive(True)
-          
-            #update the filename and label of the tab
-            tab.set_filename(filename)
-           
-            print("Loaded %s" % filename)
-        except IOError as e:          
-            print("Couldn't open file %s" % filename)
-
-def main():
-    editor = TextEditor()
+def main(filenames=[]):
+    '''
+    Creates the Text Editor object and starts GTK.
+    Accepts an optional list of filenames to pass to the Text Editor
+    '''
+    editor = TextEditor(filenames)
     editor.window.show()
     gtk.main()
 
 if __name__ == "__main__":
-    main()
+    filenames = sys.argv[1:]
+    main(filenames)
         
 
